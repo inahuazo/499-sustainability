c1fbd7f1f74ee9ba31e41714fcdb737de90e591a
Unknown <ajlabozz@yahoo.com>
2019-09-25 14:35:32 -0400

    Added new routes, development of login and goals pages.

--- CSC499-test/app/__init__.py
+++ CSC499-test/app/__init__.py
@@ -1,9 +1,11 @@
 # app/__init__.py
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import Config
 
 # Initialize app
 app = Flask(__name__)
 app.config.from_object(Config)
+Bootstrap(app)
 
 from app import routes 
--- CSC499-test/app/routes.py
+++ CSC499-test/app/routes.py
@@ -1,7 +1,34 @@
 #all page routes for application
+from flask import render_template 
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def landing():
-	return "Hello World!"
+	return render_template('landing.html')
+
+@app.route('/base')
+def base():
+	return render_template('base.html')
+	
+@app.route('/goals')
+def goals():
+	return render_template('goals.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+	
+@app.route('/events')
+def events():
+	return render_template('events.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+	
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html',form=form)
